928a9c88b343994ffbb1f6ed4b1b28d4c6939c1f
erick-q-anderson <eanderson@alegeus.com>
2024-07-23 14:03:24 -0400

    add ui project

---
+++ alegeus-ai-assist/src/app/chatbox/chatbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbox',
+  templateUrl: './chatbox.component.html',
+  styleUrls: ['./chatbox.component.css']
+})
+export class ChatboxComponent {
+
+}
